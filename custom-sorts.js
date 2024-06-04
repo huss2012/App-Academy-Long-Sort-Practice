60c60923e57e40a82327cfd776d8a4541c070e1b
huss2012 <huss24940@gmail.com>
2024-06-04 12:52:38 +0300

    All function has been written.

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,23 +1,25 @@
 function ageSort(users) {
   // Your code here
   //Using quick sort algorithm:
-  if (users.length <= 1) return users;
-  let pivot = users[0];
+  // if (users.length <= 1) return users;
+  // let pivot = users[0];
 
-  let leftUsers = [];
-  let rightUsers = [];
+  // let leftUsers = [];
+  // let rightUsers = [];
 
-  for (let i = 1; i < users.length; i++) {
-    if (users[i].age < pivot.age) {
-      leftUsers.push(users[i]);
-    } else {
-      rightUsers.push(users[i]);
-    }
-  };
-  let leftSortedUsers = ageSort(leftUsers);
-  let rightSortedUsers = ageSort(rightUsers);
+  // for (let i = 1; i < users.length; i++) {
+  //   if (users[i].age < pivot.age) {
+  //     leftUsers.push(users[i]);
+  //   } else {
+  //     rightUsers.push(users[i]);
+  //   }
+  // };
+  // let leftSortedUsers = ageSort(leftUsers);
+  // let rightSortedUsers = ageSort(rightUsers);
 
-  return leftSortedUsers.concat(pivot).concat(rightSortedUsers);
+  // return leftSortedUsers.concat(pivot).concat(rightSortedUsers);
+
+  return users.sort((a, b) => a['age'] - b['age']);
 }
 
 
@@ -76,31 +78,64 @@ const users = [
 //----------------------------------------
 function oddEvenSort(arr) {
   // Your code here
-  return arr.sort(oddEvenCompareson);
+  return arr.sort((a, b) => {
+    if (a % 2 === 1 && b % 2 === 0) return -1;
+    if (a % 2 === 0 && b % 2 === 1) return 1;
+    return a - b;
+  })
 }
-function oddEvenCompareson(a, b) {
-  if (a % 2 === 1 && b % 2 === 0) return -1;
-  if (a % 2 === 0 && b % 2 === 1) return 1;
-  return a - b;
-}
+
 //-----Local test area-----------------
 const arr1 = [5, 4, 7, 2, 9, 8, 1, 6, 3];
 const arr2 = [5, 8, 13, 6, 22, 14, 9];
-console.log(oddEvenSort(arr1));
-console.log(oddEvenSort(arr2));
+//console.log(oddEvenSort(arr1));
+//console.log(oddEvenSort(arr2));
 //--------------------------------------
 function validAnagrams(s, t) {
   // Your code here
+  if (s.length !== t.length) return false;
+
+  let sArray = s.split("").sort();
+  let tArray = t.split("").sort();
+
+  for (let i = 0; i < sArray.length; i++){
+    if (sArray[i] !== tArray[i]) return false;
+  }
+  return true;
 }
 
 function reverseBaseSort(arr) {
   // Your code here
+  return arr.sort((a, b) => {
+    const aStr = `${a}`;
+    const bStr = `${b}`;
+
+    if (aStr.length > bStr.length) return -1;
+    else if (aStr.length < bStr.length) return 1;
+
+    return a - b;
+  })
 }
 
 function frequencySort(arr) {
   // Your code here
-}
+  const frequencies = {};
 
+  for (let i = 0; i < arr.length; i++) {
+    if (!frequencies[arr[i]]) {
+      frequencies[arr[i]] = 0;
+    }
+
+    frequencies[arr[i]]++;
+  }
+
+  return arr.sort((a, b) => {
+    if (frequencies[a] < frequencies[b]) return -1;
+    else if (frequencies[a] > frequencies[b]) return 1;
+
+    return b - a;
+  });
+}
 module.exports = [
   oddEvenSort,
   validAnagrams,
